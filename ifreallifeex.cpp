31e3f84c01e1565309def1ab42a61f0322b6f47d
Nikhil Bansod <nikhilbd3@gmail.com>
2021-01-24 12:58:29 +0530

    Add files via upload

---
+++ ifreallifeex.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int a,tap=1;
+    cout<<"enter tap is on/off ie:1,0"<<endl;
+    cin>>a;
+    if(tap==a)
+    cout<<"tap is on"<<endl;
+    else
+    cout<<"tap is off"<<endl;
+
+    return 0;
+}
---
+++ insideif.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int nikhil=21,saurabh=26,z;
+    if(-5)          //in c ay non zero value is consideredd as a 1 ie:true +vr or -ve
+    cout<<"greatest is"<<saurabh<<endl;
+    else
+    cout<<"Greater is"<<nikhil;
+    return 0;
+}
