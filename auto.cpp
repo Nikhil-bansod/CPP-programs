2b0a4f56e6b88c560d8eb78b3530f2940083c811
Nikhil Bansod <nikhilbd3@gmail.com>
2021-01-24 20:57:58 +0530

    Add files via upload

---
+++ auto.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+  auto a=80;
+  auto f=3.4;  //but in c way to write is: auto int a=80;
+    cout<<a<<endl;
+    cout<<f;
+ return 0;
+
+}
---
+++ extern.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+  int a=80;  //  extern int a=80; it will gives error bcs we only declear it not defined it as per extern property
+   cout<<a;
+ return 0;
+
+}
