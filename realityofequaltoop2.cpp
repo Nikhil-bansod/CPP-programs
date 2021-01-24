0a1d114ead196db3e50cabef2805d7b70fa97ea2
Nikhil Bansod <nikhilbd3@gmail.com>
2021-01-24 14:17:59 +0530

    Add files via upload

---
+++ realityofequaltoop2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int a=3;
+   float b=3;
+    if(a==b)            //no matters what type of data type is it only compairs value of operands     
+    cout<<"equal"<<endl;
+    else
+    cout<<"not equal";
+    return 0;
+}
