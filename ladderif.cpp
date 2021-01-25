b078786df2e78af6b10db1c13224afe7c3ed9dc4
Nikhil Bansod <nikhilbd3@gmail.com>
2021-01-25 10:26:27 +0530

    Add files via upload

---
+++ ladderif.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int _savings;
+  cout<<"enter savings:"; 
+  cin>>_savings;
+ if(_savings>5000)
+ cout<<"go with shraddha";
+   else if(_savings>2000)
+      cout<<"go with prangal";
+    else
+    cout<<"go with friends";
+ return 0;
+}
--- nestedif.cpp
+++ nestedif.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
 using namespace std;
 int main()
-{   int a,b,c,d,T,P;
-cout<<"enter marks of 4 sub";
-cin>>a>>b>>c>>d;
-T=a+b+c+d;
-cout<<"T is="<<T<<endl;
-P=T*100/400;
-cout<<"Precentage is:"<<P;
-if(a>=45 && b>=45 && c>=45 && d>=45)
- cout<<"student is pass"<<endl;
-   if(P>=60)
-   cout<<"First class";
-else
- cout<<"s is fail";   
-return 0;
+{ 
+  int savings;
+  cout<<"enter savings: ";
+  cin>>savings;
+  if(savings>5000)
+     {
+       cout<<"go with shraddha"<<endl;
+       
+         if(savings>10000)
+         cout<<"go for road trip"<<endl;
+         else
+         cout<<"go for shopping";
+     }
+  else if(savings>2000)
+  cout<<"go with prangal";
+  else
+  cout<<"go with friends";
+
+ return 0;
 }
