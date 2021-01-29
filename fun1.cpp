e02ef82a5dfc7e2cc0163bb83ad6a55b9fd12973
Nikhil Bansod <nikhilbd3@gmail.com>
2021-01-29 23:28:05 +0530

    Add files via upload

---
+++ fun1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void display();
+int main(){
+    display();
+    return 0;
+}
+
+void display()
+{ 
+
+    for(int i=1;i<=100;i++)
+    cout<<i<<endl;
+
+}
